6cd17f8f6775c7935bd1d5513720e79b834012b0
DudzinDzmitry <dmitriy.i.dudin@gmail.com>
2024-01-22 18:55:35 +0300

    feat: count words and chars in text

---
+++ main.py
@@ -0,0 +1,26 @@
+def count_words(text):
+    words = text.split()
+    return len(words)
+    
+def count_letters(text):
+    letters = {}
+    fmt_text = text.lower()
+    for ch in fmt_text:
+        if ch.isalpha():
+            if ch in letters:
+                letters[ch] += 1
+            else:
+                letters[ch] = 1
+    return list(letters.items())
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(count_words(file_contents))
+        
+        letters = count_letters(file_contents) 
+        letters.sort()
+        for letter, num in letters:
+            print(f"The '{letter}' character was found {num} times")
+
+main()
