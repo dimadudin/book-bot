d3d3aede63683ec360627147abb96003ce36a041
dimadudin <dmitriy.i.dudin@gmail.com>
2024-03-24 08:47:30 +0300

    refactor: separate functionality into funcs

--- main.py
+++ main.py
@@ -1,29 +1,54 @@
-def count_words(text):
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+
+    word_num = get_word_num(text)
+    char_dict = get_char_dict(text)
+    char_list = char_dict_to_list(char_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_num} words found in the document")
+    print()
+
+    for item in char_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def get_word_num(text):
     words = text.split()
     return len(words)
 
 
-def count_letters(text):
-    letters = {}
-    fmt_text = text.lower()
-    for ch in fmt_text:
-        if ch.isalpha():
-            if ch in letters:
-                letters[ch] += 1
-            else:
-                letters[ch] = 1
-    return list(letters.items())
+def sort_on(d):
+    return d["num"]
 
 
-def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(count_words(file_contents))
+def char_dict_to_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
-        letters = count_letters(file_contents)
-        letters.sort()
-        for letter, num in letters:
-            print(f"The '{letter}' character was found {num} times")
+
+def get_char_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 
 main()
