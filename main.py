4db0f1b6edaae239bf9c4165f6d2bd2435f515b0
dimadudin <dmitriy.i.dudin@gmail.com>
2024-03-24 08:38:14 +0300

    chore: lint

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def count_words(text):
     words = text.split()
     return len(words)
-    
+
+
 def count_letters(text):
     letters = {}
     fmt_text = text.lower()
@@ -13,14 +14,16 @@ def count_letters(text):
                 letters[ch] = 1
     return list(letters.items())
 
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print(count_words(file_contents))
-        
-        letters = count_letters(file_contents) 
+
+        letters = count_letters(file_contents)
         letters.sort()
         for letter, num in letters:
             print(f"The '{letter}' character was found {num} times")
+
 
 main()
